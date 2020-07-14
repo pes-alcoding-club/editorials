711f70ef9ae0267ce14e86f57eec64af48de5ffa
souravtecken <souravtecken@gmail.com>
2020-07-14 13:09:26 +0530

    Updated editorial for CNST1

--- editorials/problems/CNST1/editorial.md
+++ editorials/problems/CNST1/editorial.md
@@ -5,10 +5,10 @@ Difficulty: Easy.
 Prerequisites: Implementation, Graph Theory, BFS.
 
 Explanation: 
-1. We need to check if a star belongs to a separate constellation or not. One way of doing that is when we encounter a star in the grid,
-   we should check if it has a star to adjacent left and just above it. A new constellation is marked when a gap is present on both sides i.e. no star. 
-   We need not check if it has a star on adjacent right or just below it(which are checked later).
-2. grid[0][0] is a special(first) case. If a star is present, raise constellation count.
-3. For edge cases, Row 1, we only check for adjacent left cell and if a star is not present, we mark it as a new constellation. Similarly for Column 1, we only check for cell just above it.
+1. The problem is basically finding the number of connected components, where 2 cells (x, y) and (xx, yy) are said to be connected 
+only if `x + 1 = xx` or `x - 1 = xx` or `y + 1 = yy` or `y - 1 = yy`
+2. This can be achieved by a simple BFS on the grid.
+3. Traverse through the array, on every occurence of a '1', perform a BFS with that cell as the root, and delete that particular component from the grid. This can be done either by using a visited array, or just marking the visited cells as 0 in the grid. 
+4. The number of BFSs carried out will provide the value of the number of constallations/components.
 
 Time Complexity: O(m*n)
--- editorials/problems/CNST1/solution.cpp
+++ editorials/problems/CNST1/solution.cpp
@@ -1,44 +1,48 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int t;
-    cin>>t;
-    int m;
-    int n;
-    int a[1005][1005] = {{0}};
-    for(int test=0;test<t;++test)
-    {
-        cin>>m>>n;        
-        for(int i=0;i<m;++i)
-        {
-            for(int j=0;j<n;++j)
-            {
-                cin>>a[i][j];
-            }
+
+int a[1005][1005];
+
+int isValid(int i, int j, int m, int n){
+    return i >= 0 && i < m && j >= 0 && j < n;
+}
+
+void bfs(int i, int j, int m, int n){
+    int directions[4][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+    queue<pair<int, int> > q;    
+    q.push({i, j});    
+    a[i][j] = 0;
+    while(!q.empty()) {
+        pair<int, int> node = q.front();        
+        for(int k=0; k<4; ++k){
+            int ii = node.first + directions[k][0];
+            int jj = node.second + directions[k][1];
+            if(isValid(ii, jj, m, n) && a[ii][jj])
+                q.push({ii, jj}), a[ii][jj] = 0;
         }
-        int count=0;
-        for(int i=0;i<m;++i)
-        {
-            for(int j=0;j<n;++j)
-            {
-                if(a[i][j])
-                {
-                    if(a[i][j]==1){
-                        if(i==0 && j==0)
-                            ++count;
-                        else if(i == 0 && a[i][j-1] == 0)
-                            ++count;
-                        else if(j==0 && a[i-1][j] == 0)
-                            ++count;                            
-                        else if(a[i-1][j] == 0 && a[i][j-1] == 0)
-                            ++count;                        
-                    }                                            
-                }
-            }
-        }
-        cout<<count<<endl;
+        q.pop();
     }
+}
+
+void solve() {
+    int m, n;
+    cin >> m >> n;
+    for(int i=0; i<m; ++i)
+        for(int j=0; j<n; ++j)
+            cin >> a[i][j];
+    int count = 0;
+    for(int i=0; i<m; ++i)
+        for(int j=0; j<n; ++j)
+            if(a[i][j])
+                bfs(i, j, m, n), ++count;
+    cout<<count<<"\n";        
+}
+
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
     return 0;
 }
